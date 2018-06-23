94ccaa7c0cbf320fa5db6f996edb55bc5a309d6c
listen <xuexine@sina.cn>
2018-06-23 19:04:00 +0800

    2018-6-23

--- CRUD/src/main/java/com/listen/sql/demo.java
+++ CRUD/src/main/java/com/listen/sql/demo.java
@@ -1,4 +1,34 @@
 package com.listen.sql;
 
-public class demo {
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class demo extends AbstractVerticle {
+
+
+    @Override
+    public void start() throws Exception {
+        vertx.setPeriodic(100L,it -> {
+            try {
+                Robot robot = new Robot();
+                robot.keyPress(KeyEvent.VK_1);
+                Thread.sleep(10L);
+                robot.keyPress(KeyEvent.VK_ESCAPE);
+                robot.keyPress(KeyEvent.VK_ALT);
+
+            } catch (AWTException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new demo());
+    }
 }
